4253593c87a28b002a9d01cb3b96498dbe7847a8
ThanhZ <89237062+thqnhz@users.noreply.github.com>
2025-09-01 15:04:31 +0700

    feat: drew row and col labels

--- src/Game/Chess.c
+++ src/Game/Chess.c
@@ -10,16 +10,19 @@ void Run() {
         BeginDrawing();
         ClearBackground(DARKGREEN);
         DrawBoard(&game);
+        DrawColLabels(&game);
+        DrawRowLabels(&game);
         EndDrawing();
     }
     CloseWindow();
 }
 
 void InitGame(Game *game) {
-    game->squareSize = (GetScreenHeight() - 40) / 8.0f;
+    game->squareSize = (GetScreenHeight() - 80) / 8.0f;
+    game->font = LoadFontEx(TextFormat("%s/asset/font/Inter/static/Inter_28pt-Regular.ttf", GetWorkingDirectory()), 256, 0, 0);
     for (auto i = 0; i < 8; i++) {
         for (auto j = 0; j < 8; j++) {
-            Rectangle r = { .x = i*game->squareSize + 20, .y = j*game->squareSize + 20, .height = game->squareSize, .width = game->squareSize };
+            Rectangle r = { .x = i*game->squareSize + 40, .y = j*game->squareSize + 40, .height = game->squareSize, .width = game->squareSize };
             game->board[i][j] = r;
         }
     }
@@ -31,5 +34,28 @@ void DrawBoard(Game *game) {
             Color color = (i + j) % 2 == 0 ? LIGHTGRAY : GRAY;
             DrawRectangleRec(game->board[i][j], color);
         }
+    }
+}
+
+void DrawRowLabels(Game *game) {
+    for (auto j = 0; j < 8; j++) {
+        const char *colLabel = TextFormat("%d", -j + 8);
+        Vector2 labelSize = MeasureTextEx(game->font, colLabel, 25, 1);
+        Vector2 pos = {
+            game->board[0][j].x - labelSize.x - 5,
+            game->board[0][j].y + game->squareSize / 2 - labelSize.y / 2
+        };
+        DrawTextEx(game->font, colLabel, pos, 25, 1, BLACK);
+    }
+}
+
+void DrawColLabels(Game *game) {
+    const char *rowLabels = "abcdefgh";
+    for (auto i = 0; i < 8; i++) {
+        Vector2 pos = {
+            game->board[i][7].x + game->squareSize / 2 - MeasureTextEx(game->font, TextFormat("%c", rowLabels[i]), 25, 1).x / 2.0f,
+            game->board[i][7].y + game->squareSize
+        };
+        DrawTextEx(game->font, TextFormat("%c", rowLabels[i]), pos, 25, 1, BLACK);
     }
 }
--- src/Game/Chess.h
+++ src/Game/Chess.h
@@ -6,6 +6,7 @@
 typedef struct Game {
     float squareSize;
     Rectangle board[8][8];
+    Font font;
 } Game;
 
 void Run();
@@ -13,3 +14,7 @@ void Run();
 void InitGame(Game *game);
 
 void DrawBoard(Game *game);
+
+void DrawColLabels(Game *game);
+
+void DrawRowLabels(Game *game);
